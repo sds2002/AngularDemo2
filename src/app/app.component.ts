7024f7f8562fbb793d92fa520b0fe207e208c350
UKX0502 <deepa.balasubramanian@infotel.com>
2025-09-02 14:54:19 +0530

    validations in template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  loginForm: FormGroup;
+  user = {
+    name: '',
+    email: '',
+    password: ''
+  };
 
-  constructor(private fb: FormBuilder) {
-    // Define form controls with validators
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
-
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log('Form Submitted ✅', this.loginForm.value);
-    } else {
-      console.log('Form Invalid ❌');
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Submitted:', this.user);
+      alert('Registration Successful!');
     }
   }
 }
