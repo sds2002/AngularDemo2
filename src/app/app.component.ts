c3cba8d980b71f7baed8dcf1355acad6b6b2265d
UKX0502 <deepa.balasubramanian@infotel.com>
2025-09-02 12:15:09 +0530

    Service Demo using HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: []
 })
-export class AppComponent {
-  title = 'AngularDemo';
+export class AppComponent implements OnInit {
+  posts: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
 }
